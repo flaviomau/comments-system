58dcbe8a7464ef8259257eb8ea9f7740c4790346
flaviomau <flaviomau@yahoo.com>
2018-06-15 15:39:57 -0300

    created Comments component and internal state of app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,37 @@ import React, { Component } from 'react'
 import 'bootstrap-css-only'
 
 import NewComment from './NewComment'
+import Comments from './Comments'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      comments:{
+        1: {
+          comment: 'first comment'
+        },
+        2: {
+          comment: 'second comment'
+        }
+      }
+    }
+  }
+
+  postNewComment(comment){
+    this.setState({
+      comments:{
+        ...this.state.comments, comment
+      }
+    })
+  }
+
   render() {
     return (
       <div className="container">
-        <NewComment/>
+        <NewComment postNewComment={this.postNewComment}/>
+        <Comments comments={this.state.comments}/>
       </div>
     )
   }
---
+++ src/Comments.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Comments extends Component{
+    render(){
+        return(
+            <div>
+                {JSON.stringify(this.props.comments)}
+            </div>
+        )
+    }
+}
+
+export default Comments
